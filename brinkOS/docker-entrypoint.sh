634a83f94a1ccdee7f01958b4fe88f45e21e8eb1
James Brink <brink.james@gmail.com>
2018-04-25 23:42:49 -0700

    Enabled Graphical Login

--- brinkOS/docker-entrypoint.sh
+++ brinkOS/docker-entrypoint.sh
@@ -3,8 +3,7 @@
 if [ "$#" -eq 0 ]; then
 	echo "Building brinkOS"
 	cd /build/archlive/ || exit
-	./build.sh -v -N brinkOS -o /iso &	
-	wait
+	./build.sh -v -N brinkOS -o /iso
 	echo "Exiting"
 else
 	exec "$@"
