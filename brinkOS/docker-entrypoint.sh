17edcb947224340593853728c9199a2c79e396d7
James Brink <brink.james@gmail.com>
2018-04-26 03:32:09 -0700

    Added time to track build time

--- brinkOS/docker-entrypoint.sh
+++ brinkOS/docker-entrypoint.sh
@@ -3,7 +3,7 @@
 if [ "$#" -eq 0 ]; then
 	echo "Building brinkOS"
 	cd /build/archlive/ || exit
-	./build.sh -v -N brinkOS -o /iso
+	time ./build.sh -v -N brinkOS -o /iso
 	echo "Exiting"
 else
 	exec "$@"
