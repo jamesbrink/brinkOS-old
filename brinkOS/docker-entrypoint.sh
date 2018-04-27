74beaeb812b7cdb9d3fee5fbc12244ea22cf292a
James Brink <brink.james@gmail.com>
2018-04-26 19:28:09 -0700

    Added a keep_alive method to stop travis-ci builds from dying

--- brinkOS/docker-entrypoint.sh
+++ brinkOS/docker-entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+# Used to keep travis from timing out.
+keep_alive(){
+	while kill -0 $1 > /dev/null 2>&1; do
+		echo "$(date) Ping!"
+		sleep 300
+	done
+}
+
 if [ "$#" -eq 0 ]; then
 	echo "Building brinkOS"
 	cd /build/archlive/ || exit
-	./build.sh -v -N brinkOS -o /iso
+	./build.sh -v -N brinkOS -o /iso &
+	build_pid=$!
+	keep_alive $build_pid
 	exit $?
 else
 	exec "$@"
